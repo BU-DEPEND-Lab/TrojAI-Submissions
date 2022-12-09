7634da9d479f80e0129c7d3c3927b2d138324539
pkiourti <pkiourti@gmail.com>
2022-12-09 09:51:04 -0500

    update features and remove sizecap in analyzer weight

--- analyzer_weight.py
+++ analyzer_weight.py
@@ -5,6 +5,7 @@ import time
 import json
 import jsonschema
 import jsonpickle
+import pandas
 
 import math
 from sklearn.cluster import AgglomerativeClustering
@@ -15,33 +16,16 @@ import util.smartparse as smartparse
 import helper_r10 as helper
 import engine_objdet as engine
 
-
-#value at top x percentile, x in 0~100
-def hist_v(w,bins=100):
-    s,_=w.sort(dim=0);
-    wmin=float(w.min());
-    wmax=float(w.max());
-
-    n=s.shape[0];
-    hist=torch.Tensor(bins);
-    for i in range(bins):
-        x=math.floor((n-1)/(bins-1)*i)
-        x=max(min(x,n),0);
-        v=float(s[x]);
-        hist[i]=v;
-
-    return hist;
-
-def analyze(param, k, szcap=4096):
+def analyze(param, k):
     if len(param.shape)==1:
-        fvs=analyze(param.view(1,-1), k, szcap=szcap)
+        fvs=analyze(param.view(1,-1), k)
         return fvs
     if len(param.shape)==4:
         #Conv filters
         fvs=[];
         for i in range(param.shape[-2]):
             for j in range(param.shape[-1]):
-                fv=analyze(param[:,:,i,j], k, szcap=szcap);
+                fv=analyze(param[:,:,i,j], k);
                 fvs=fvs+fv;
 
         return fvs
@@ -58,6 +42,7 @@ def analyze(param, k, szcap=4096):
 
 #         print('before svd')
         U, S, V = param.svd()
+        # S = S / torch.linalg.norm(S)
 #         print('after svd')
         # e,_=z.eig();
         # eig_norm = torch.linalg.norm(e, dim=1)
@@ -81,11 +66,11 @@ def analyze(param, k, szcap=4096):
     else:
         return [];
 
-def run(interface, k, szcap=4096):
+def run(interface, k):
     fvs=[];
     for param in interface.model.parameters():
         #print('fvs', len(fvs))
-        fvs=fvs+analyze(param.data, k, szcap=szcap);
+        fvs=fvs+analyze(param.data, k);
 
     #print()
     #print('fvs shape', len(fvs), len(fvs[0]), len(fvs[49]))
@@ -97,9 +82,11 @@ def run(interface, k, szcap=4096):
 
     min_features = torch.min(fvs, dim=0).values
     max_features = torch.max(fvs, dim=0).values
-    mean_features = torch.mean(fvs, dim=0)
-    std_features = torch.std(fvs, dim=0)
-    fvs = torch.cat((min_features, max_features, mean_features, std_features), dim=0)
+    quantiles = torch.quantile(fvs, q=torch.Tensor([0.1, 0.9]).cuda(), dim=0)
+    # mean_features = torch.mean(fvs, dim=0)
+    # std_features = torch.std(fvs, dim=0)
+    # fvs = torch.cat((min_features, max_features, ), dim=0)
+    fvs = torch.cat((min_features.unsqueeze(0), max_features.unsqueeze(0), quantiles), dim=0)
 
     return fvs;
 
@@ -109,30 +96,35 @@ def run(interface, k, szcap=4096):
 def extract_fv(id=None, model_filepath=None, scratch_dirpath=None, examples_dirpath=None, params=None):
     t0=time.time();
     default_params=smartparse.obj();
-    default_params.szcap=4096
     params = smartparse.merge(params,default_params);
 
     if not id is None:
         model_filepath, scratch_dirpath, examples_dirpath=helper.get_paths(id, root = '/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round11/');
 
     interface=engine.new(model_filepath);
-    fvs=run(interface, 3, szcap=params.szcap)
+    fvs=run(interface, 1)
 
     print('Weight analysis done, time %.2f'%(time.time()-t0))
     return fvs
 
 if __name__ == "__main__":
-    data=db.Table({'model_id':[],'model_name':[],'fvs':[]}); #,'label':[] label currently incorrect
+    data=db.Table({'model_id':[],'model_name':[],'fvs':[], 'label': []}); #,'label':[] label currently incorrect
     data=db.DB({'table_ann':data});
 
     t0=time.time()
 
     default_params=smartparse.obj();
-    default_params.szcap=4096;
-    default_params.fname='data_r11_k3_20_features.pt'
+    default_params.fname='test.pt'
     params=smartparse.parse(default_params);
     params.argv=sys.argv
     data.d['params']=db.Table.from_rows([vars(params)]);
+
+
+    meta = pandas.read_csv('/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round11/METADATA.csv')
+    poisoned = {}
+    for i in range(len(meta['model_name'])):
+        poisoned[meta['model_name'][i]] = meta['poisoned'][i]
+        
 
     model_ids=list(range(0,288))
 
@@ -152,17 +144,19 @@ if __name__ == "__main__":
 
             f.close();
             '''
-
             data['table_ann']['model_name'].append('id-%08d'%id);
             data['table_ann']['model_id'].append(id);
             #data['table_ann']['label'].append(label);
             data['table_ann']['fvs'].append(fv);
+            data['table_ann']['label'].append(int(poisoned['id-%08d'%id]))
 
+            # print(fv)
             print('Model %d(%d), time %f'%(i,id,time.time()-t0));
             if i%1==0:
                 data.save(params.fname);
         except AttributeError as e:
             print(f"Skip loading Model-{id}: {e}")
 
+    print(data['table_ann']['fvs'])
     data.save(params.fname);
 
