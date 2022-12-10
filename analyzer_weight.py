8884eb01aa77c4009e4f692f13dcfbeffea13d90
zwc662 <zwc662@gmail.com>
2022-12-09 20:29:45 -0500

    update

--- analyzer_weight.py
+++ analyzer_weight.py
@@ -127,7 +127,7 @@ def analyze(param,nbins=100,szcap=4096):
         bot_k_ids = indices_norm_based_desc[:10]
         bot_k_s = S[bot_k_ids].flatten()
          
-        fv=torch.cat((e_mean.cpu(), e_std.cpu(), e_norm.cpu(), top_k_e.cpu(), bot_k_e.cpu(), s_mean.cpu(), s_std.cpu(), s_norm.cpu(), top_k_s.cpu(), bot_k_s.cpu(), e2_hist,er_hist,ec_hist,eig_persist,w_hist,wabs_hist),dim=0);
+        fv=torch.cat((mat_norm.cpu(), e_mean.cpu(), e_std.cpu(), e_norm.cpu(), top_k_e.cpu(), bot_k_e.cpu(), s_mean.cpu(), s_std.cpu(), s_norm.cpu(), top_k_s.cpu(), bot_k_s.cpu(), e2_hist,er_hist,ec_hist,eig_persist,w_hist,wabs_hist),dim=0);
         return [fv];
     else:
         return [];
@@ -139,8 +139,6 @@ def run(interface,nbins=100,szcap=4096):
 
     fvs=torch.stack(fvs);
     return fvs;
-
-
 
 #Fuzzing call for TrojAI R9
 def extract_fv(id=None, model_filepath=None, scratch_dirpath=None, examples_dirpath=None, params=None):
