08fc0ca9401b5edf6ad32527822124d781a2bc39
zwc662 <zwc662@gmail.com>
2022-12-11 02:26:18 -0500

    udpate

--- arch/mlp_set_eig.py
+++ arch/mlp_set_eig.py
@@ -74,7 +74,7 @@ class new(nn.Module):
         for i in range(b):
             h_i=self.encoder_hist(weight_dist[i].cuda());
             h_i=torch.quantile(h_i,self.q,dim=0).contiguous().view(-1);
-            h_i=self.encoder_combined(h_i);
+            h_i=self.encoder_combined(h_i).squeeze(0);
             h.append(h_i);
         
         h=torch.stack(h,dim=0);
