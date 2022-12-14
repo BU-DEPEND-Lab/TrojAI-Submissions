75373d000e33795e130a76adc11864c80bd970a7
zwc662 <zwc662@gmail.com>
2022-12-13 20:47:36 -0500

    update

--- crossval_folds.py
+++ crossval_folds.py
@@ -330,12 +330,12 @@ def run_crossval(p):
     #print(torch.tensor(tot), torch.sum(torch.tensor(tot)))
     acc = torch.sum(torch.tensor(true_preds)) / torch.sum(torch.tensor(tots))
     cepre=torch.Tensor(cepre);
-    if float(auc.mean()) > best_auc_so_far:
-        best_auc_so_far=float(auc.mean());
-        torch.save(ensemble,session.file('model.pt'))
-    #if float(cepre.mean())<best_loss_so_far:
-    #    best_loss_so_far=float(cepre.mean());
+    #if float(auc.mean()) > best_auc_so_far:
+    #    best_auc_so_far=float(auc.mean());
     #    torch.save(ensemble,session.file('model.pt'))
+    if float(cepre.mean())<best_loss_so_far:
+        best_loss_so_far=float(cepre.mean());
+        torch.save(ensemble,session.file('model.pt'))
 
     session.log('AUC: %f + %f, ACC: %f, CE: %f + %f, CEpre: %f + %f (%s (%d,%d,%d), epochs %d, batch %d, lr %f, decay %f)'%(auc.mean(),2*auc.std(),acc, ce.mean(),2*ce.std(),cepre.mean(),2*cepre.std(),arch,nlayers,nlayers2,nh,epochs,batch,lr,decay));
 
