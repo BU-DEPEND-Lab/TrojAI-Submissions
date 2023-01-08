c249f921cbcee827923bec103901b11d127685f8
zwc662 <zwc662@gmail.com>
2023-01-08 01:17:34 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ python entrypoint.py infer \
 --learned_parameters_dirpath ./learned_parameters \
 --metaparameters_filepath ./metaparameters.json \
 --schema_filepath=./metaparameters_schema.json \
---scale_parameters_filepath ./scale_params.npy
+--scale_parameters_filepath ./learned_parameters/scale_params.npy
 
 #python trojan_detector.py  --model_filepath=./data/round9-train-dataset/models/id-00000105/model.pt  --tokenizer_filepath=./data/round9-train-dataset/tokenizers/google-electra-small-discriminator.pt  --features_filepath=./features.csv  --result_filepath=./output.txt  --scratch_dirpath=./scratch/  --examples_dirpath=./data/round9-train-dataset/models/id-00000105/clean-example-data.json  --round_training_dataset_dirpath=/path/to/training/dataset/  --metaparameters_filepath=./metaparameters.json  --schema_filepath=./metaparameters_schema.json  --learned_parameters_dirpath=./learned_parameters
 
@@ -19,16 +19,16 @@ sudo singularity build example_trojan_detector.simg example_trojan_detector.def
 
 #singularity run --nv -B /mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round11:/data/ test-trojai-r10-weight-v2.simg --learned_parameters_dirpath=./learned_parameters --model_filepath=/data/models/id-00000003/model.pt  --result_filepath=./output.txt  --scratch_dirpath=./scratch/  --examples_dirpath=/data/models/id-00000003/clean-example-data.json --source_dataset_dirpath /data/image-classification-sep2022-example-source-dataset  --round_training_dataset_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round11/models  --metaparameters_filepath=/metaparameters.json  --schema_filepath=/metaparameters_schema.json
  singularity run \
---bind /mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round12/trojai_example \
+--bind /mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round12/ \
 --nv \
 ./example_trojan_detector.simg \
 infer \
---model_filepath=./model/id-00000002/model.pt \
---metaparameters_filepath=./metaparameters.json \
---schema_filepath=./metaparameters_schema.json \
+--model_filepath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round12/models/id-00000002/model.pt \
+--metaparameters_filepath=/metaparameters.json \
+--schema_filepath=/metaparameters_schema.json \
 --result_filepath=./output.txt \
 --scratch_dirpath=./scratch/ \
---examples_dirpath=./model/id-00000002/clean-example-data/ \
---round_training_dataset_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round12/trojai_example/ \
---learned_parameters_dirpath=./learned_parameters/ \
---scale_parameters_filepath ./scale_params.npy
+--examples_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round12/models/id-00000002/clean-example-data/ \
+--round_training_dataset_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round12/ \
+--learned_parameters_dirpath=/learned_parameters/ \
+--scale_parameters_filepath=/learned_parameters/scale_params.npy
