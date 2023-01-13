0c6eb00f19e233176c47bd3b57fcd61deb9ecccc
zwc662 <zwc662@gmail.com>
2023-01-13 10:28:03 -0500

    update

--- detector.py
+++ detector.py
@@ -342,7 +342,9 @@ class Detector(AbstractDetector):
         logging.info(f"Flattened grads: {[weights for (layer, weights) in flat_grads[0].items()]}")
         #grad_layer_transform = fit_feature_reduction_algorithm({model_class: flat_grads}, self.weight_table_params, self.ICA_features)
         grad_layer_transform = {model_class: None}
+        print(len(flat_grads))
         grad_model_transform = stat_feature_reduction_algorithm({model_class: flat_grads}, self.input_features)
+        print(len(flat_grads))
         logging.info("Grad transformer fitted")
         X = (
             np.hstack(\
