34cbe03cf1cac532b133a51fc431ec4d6e163617
zwc662 <zwc662@gmail.com>
2023-01-14 02:17:00 -0500

    update

--- feature_extractor.py
+++ feature_extractor.py
@@ -131,10 +131,12 @@ class FeatureExtractor(object):
         #del model_repr_dict
         logging.info("Models flattened. Fitting weight feature reduction...")
         layer_transform = fit_ICA_feature_reduction_algorithm(flat_models, self.weight_table_params, self.ICA_features)
+        logging.info("Weight feature reduction done...")
         #del flat_models
         flat_clean_grad_repr_dict = {}
         flat_poisoned_grad_repr_dict= {}
         for (model_class, models) in model_repr_dict:
+            print(model_class)
             flat_clean_grad_repr_dict[model_class] = []
             #flat_poisoned_grad_repr_dict[model_class] = []
             for i, model in enumerate(models):
@@ -149,7 +151,7 @@ class FeatureExtractor(object):
         
         logging.info("Models flattened. Fitting grad feature reduction...")
         clean_grad_layer_transform = fit_ICA_feature_reduction_algorithm(flat_clean_grad_repr_dict, self.weight_table_params, self.ICA_features)
-        
+        logging.info("Grad feature reduction done...")
         #poisoned_grad_layer_transform = fit_ICA_feature_reduction_algorithm(flat_poisoned_grad_repr_dict, self.weight_table_params, self.ICA_features)
         #logging.info("Models flattened. Fitting feature reduction...")
         
