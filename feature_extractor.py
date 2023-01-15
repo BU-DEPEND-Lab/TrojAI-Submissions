42f50820073c1f8561b1cd1791d3ae73956573b0
zwc662 <zwc662@gmail.com>
2023-01-15 03:20:07 -0500

    updae

--- feature_extractor.py
+++ feature_extractor.py
@@ -184,7 +184,7 @@ class FeatureExtractor(object):
                 #    poisoned_grad_layer_transform[model_class], flat_poisoned_grad_repr_dict[model_class][i]
                 #)
 
-                feats = np.hstack((model_feats, clean_grad_feats)).tolist()#, poisoned_grad_feats)).tolist()
+                feats = np.hstack((model_feats, clean_grad_feats))#, poisoned_grad_feats)).tolist()
                 logging.info(f" ICA feature size: {feats.shape}\n")
                 feats = np.pad(feats, [(0, 0), (0, 2 * self.ICA_features - feats.shape[-1])], mode='constant')
                 assert feats.shape[-1] == 2 * self.ICA_features
