b409b00d01639824fbb072bdfc153f2dbedfb408
zwc662 <zwc662@gmail.com>
2023-07-31 18:17:55 -0400

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-## For inference
+ ## For inference
 python entrypoint.py infer \
 --model_filepath ./model/rl-lavaworld-jul2023-example/model.pt \
 --result_filepath ./output.txt \
 --scratch_dirpath ./scratch \
 --examples_dirpath ./model/rl-lavaworld-jul2023-example/clean-example-data \
---round_training_dataset_dirpath / \
+--round_training_dataset_dirpath /mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round14/rl-lavaworld-jul2023-train/models \
 --learned_parameters_dirpath ./new_learned_parameters \
 --metaparameters_filepath ./metaparameters.json \
 --schema_filepath=./metaparameters_schema.json
@@ -17,4 +17,4 @@ python entrypoint.py configure \
 --metaparameters_filepath=./metaparameters.json \
 --schema_filepath=./metaparameters_schema.json \
 --learned_parameters_dirpath=./new_learned_parameters/ \
---configure_models_dirpath=/path/to/new-train-dataset
+--configure_models_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round14/rl-lavaworld-jul2023-train/models
