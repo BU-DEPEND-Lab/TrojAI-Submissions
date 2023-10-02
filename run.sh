fb0be1ba3c3ece80a57f945eae5b3859c4219aa0
zwc662 <zwc662@gmail.com>
2023-10-02 15:59:29 -0400

    update

--- run.sh
+++ run.sh
@@ -8,24 +8,24 @@ then
     then
         ## For inference
         python entrypoint.py infer \
-        --model_filepath ./model/rl-lavaworld-jul2023-example/model.pt \
+        --model_filepath /home/zwc662/Workspace/TrojAI-Submissions/model/id-00000000/model.pt \
         --result_filepath ./output.txt \
         --scratch_dirpath ./scratch \
-        --examples_dirpath ./model/rl-lavaworld-jul2023-example/clean-example-data \
+        --examples_dirpath /home/zwc662/Workspace/TrojAI-Submissions/model/id-00000000/clean-example-data/ \
         --round_training_dataset_dirpath /mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round14/rl-lavaworld-jul2023-train/models \
         --learned_parameters_dirpath ./new_learned_parameters \
         --metaparameters_filepath ./metaparameters.json \
         --schema_filepath=./metaparameters_schema.json
     else 
         singularity run \
-        --bind /mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round14/rl-lavaworld-jul2023-train/models \
+        --bind /home/zwc662/Workspace/TrojAI-Submissions/model/ \
         --nv \
         ./detector.simg \
         infer \
-        --model_filepath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round14/rl-lavaworld-jul2023-train/models/id-00000000/model.pt \
+        --model_filepath=/home/zwc662/Workspace/TrojAI-Submissions/model/id-00000000/model.pt \
         --result_filepath=./output.txt \
         --scratch_dirpath=./scratch/ \
-        --examples_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round14/rl-lavaworld-jul2023-train/models/id-00000000/clean-example-data/ \
+        --examples_dirpath=/home/zwc662/Workspace/TrojAI-Submissions/model/id-00000000/clean-example-data/ \
         --round_training_dataset_dirpath=/path/to/training/dataset/ \
         --metaparameters_filepath=/metaparameters.json \
         --schema_filepath=/metaparameters_schema.json \
