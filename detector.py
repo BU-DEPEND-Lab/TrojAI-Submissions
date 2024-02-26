6eb843cec84de0d05f44ca7e39273a934f8ff7b7
zwc662 <zwc662@gmail.com>
2024-02-26 18:45:48 -0500

    update

--- detector.py
+++ detector.py
@@ -407,7 +407,7 @@ class Detector(AbstractDetector):
                 'model_schema': {
                     'classifier': {
                         'name': 'TrafficNN', 
-                        'load_from_file': os.path.join(os.path.dirname(__file__), 'best_attr_cls_1.p')
+                        'load_from_file': os.path.join(os.path.dirname(__file__), 'best_attr_cls_2.p')
                     },
                     'save_dir': 'best_attr_cls_2_2.p'
                 },
