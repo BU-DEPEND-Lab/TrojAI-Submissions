6b9b4c84c4f61e154fe13f209e9238ee80004466
zwc662 <zwc662@gmail.com>
2024-06-01 14:48:00 -0400

    update

---
+++ run.sh
@@ -0,0 +1,48 @@
+--examples_dirpath model/models/id-00000001/ --round_training_dataset_dirpath ./model/models/ --metaparameters_filepath ./metaparameters.json --schema_filepath ./metaparameters_schema.json --learned_parameters_dirpath ./learned_parameters/
+
+#!/bin/bash
+
+
+if [[ -n $1 ]]
+then 
+
+    if [[ $1 = infer ]]
+    then
+        ## For inference
+        python entrypoint.py infer \
+        --model_filepath ./model/models/id-00000000/ \
+        --result_filepath ./output.txt \
+        --scratch_dirpath ./scratch \
+        --examples_dirpath ./model/models/id-00000001/clean-example-data/ \
+        --round_training_dataset_dirpath ./model/ \
+        --learned_parameters_dirpath ./new_learned_parameters \
+        --metaparameters_filepath ./metaparameters.json \
+        --schema_filepath=./metaparameters_schema.json
+    else 
+        singularity run \
+        --bind ~/Workspace/TrojAI-Submissions/model/ \
+        --nv \
+        ./detector.simg \
+        infer \
+        --model_filepath=./model/models/id-00000000/ \
+        --result_filepath=./output.txt \
+        --scratch_dirpath=./scratch/ \
+        --examples_dirpath=./model/models/id-00000001/clean-example-data/ \
+        --round_training_dataset_dirpath=/path/to/training/dataset/ \
+        --metaparameters_filepath=/metaparameters.json \
+        --schema_filepath=/metaparameters_schema.json \
+        --learned_parameters_dirpath=/learned_parameters/
+    fi
+else
+    ## For configuration
+    python entrypoint.py configure \
+    --scratch_dirpath=./scratch/ \
+    --metaparameters_filepath=./metaparameters.json \
+    --schema_filepath=./metaparameters_schema.json \
+    --learned_parameters_dirpath=./new_learned_parameters/ \
+    --configure_models_dirpath=./model
+    #--configure_models_dirpath=/mnt/md0/shared/TrojAI-Submissions/trojai-datasets/round16/cyber-apk-nov2023-train/models
+fi
+
+
+#scp rl-lavaworld-jul2023_test_TrinitySRITrojAI-BostonU.simg weichaozhou@168.122.70.64:~/Downloads/
